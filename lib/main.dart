04997f1cb7ec72b87f8041329d905fb66c53e14f
menamiji <menamiji@gmail.com>
2025-09-05 08:42:06 +0900

    2번째

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: '타틀'),
+      home: const MyHomePage(title: '타이틀'),
     );
   }
 }
